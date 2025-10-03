e7f10e098b4687484e263a412de97d51a12d1949
ST10388547 <127096494+ST10388547@users.noreply.github.com>
2025-10-03 15:32:01 +0200

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,7 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GiftOfTheGivers.Data;
 
 public class HomeController : Controller
 {
-    public IActionResult Index() => View();
-    public IActionResult About() => View();
+    private readonly ApplicationDbContext _db;
+
+    public HomeController(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var eventsList = await _db.Events
+            .Include(e => e.Project)
+            .OrderBy(e => e.Date)
+            .ToListAsync();
+
+        return View(eventsList);
+    }
 }
+
+
